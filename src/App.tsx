f31157b4c914033ec5beb98d2312199dd09aa09f
Gordon Chan <altg.star@gmail.com>
2022-03-19 16:43:25 +0800

    update: revamp menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Tutorial, KeyPractice, DecodePracticeIndex, ArticlePracticeIndex } from "./views";
 import { BasicKeyPractice, RootKeyPractice } from "./views/KeyPractice";
 import { AssociateCode, CommonHead, CommonTail, SingleCharacter, Word } from "./views/DecodePractice";
 
+//style
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
+
 const App = (): React.ReactElement => {
 	return (
-		<HashRouter basename={process.env.PUBLIC_URL}>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/tutorial" element={<Tutorial />} />
-				<Route path="/press-key-practice" element={<KeyPractice />} />
-				<Route path="/key-practice/basic" element={<BasicKeyPractice />} />
-				<Route path="/key-practice/root/:question" element={<RootKeyPractice />} />
-				<Route path="/decode-practice" element={<DecodePracticeIndex />} />
-				<Route path="/decode-practice/associate" element={<AssociateCode />} />
-				<Route path="/decode-practice/common-head" element={<CommonHead />} />
-				<Route path="/decode-practice/common-tail" element={<CommonTail />} />
-				<Route path="/decode-practice/single-character" element={<SingleCharacter />} />
-				<Route path="/decode-practice/word" element={<Word />} />
-				<Route path="/article-practice" element={<ArticlePracticeIndex />} />
-			</Routes >
-		</HashRouter>
-	)};
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/tutorial" element={<Tutorial />} />
+					<Route path="/press-key-practice" element={<KeyPractice />} />
+					<Route path="/key-practice/basic" element={<BasicKeyPractice />} />
+					<Route path="/key-practice/root/:question" element={<RootKeyPractice />} />
+					<Route path="/decode-practice" element={<DecodePracticeIndex />} />
+					<Route path="/decode-practice/associate" element={<AssociateCode />} />
+					<Route path="/decode-practice/common-head" element={<CommonHead />} />
+					<Route path="/decode-practice/common-tail" element={<CommonTail />} />
+					<Route path="/decode-practice/single-character" element={<SingleCharacter />} />
+					<Route path="/decode-practice/word" element={<Word />} />
+					<Route path="/article-practice" element={<ArticlePracticeIndex />} />
+				</Routes>
+			</BrowserRouter>
+		</ThemeProvider>
+	)
+};
 export default App;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,17 +1,69 @@
 import React from "react";
-import { IndexContainer } from "../components";
+
+// components
+import { Container, Typography, Grid, Box } from "@mui/material";
+import { Card, CardActionArea, CardContent } from "@mui/material";
+// icons
+import { SvgIconComponent } from "@mui/icons-material";
+import MenuBookIcon from "@mui/icons-material/MenuBook"; // tutorial;
+import KeyboardOutlinedIcon from "@mui/icons-material/KeyboardOutlined"; //key-practice
+import TranslateOutlinedIcon from "@mui/icons-material/TranslateOutlined"; //decode-practice
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined"; //article-practice
+import SpeedOutlinedIcon from "@mui/icons-material/SpeedOutlined"; //speed-test
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined"; //about
 
 const linkList = [
-    { "title": "基礎教學", "href": "/tutorial" },
-    { "title": "按鍵練習", "href": "/key-practice" },
-    { "title": "折碼練習", "href": "/decode-practice" },
-    { "title": "文章練習", "href": "/article-practice" },
-    { "title": "速度測試", "href": "/tutorial" },
+    { "title": "基礎教學", "href": "/tutorial", "icon": MenuBookIcon },
+    { "title": "按鍵練習", "href": "/key-practice", "icon": KeyboardOutlinedIcon },
+    { "title": "折碼練習", "href": "/decode-practice", "icon": TranslateOutlinedIcon },
+    { "title": "文章練習", "href": "/article-practice", "icon": ArticleOutlinedIcon },
+    { "title": "速度測試", "href": "/tutorial", "icon": SpeedOutlinedIcon },
+    { "title": "關於", "href": "/about", "icon": InfoOutlinedIcon },
 ]
+
+type MenuCardProps = {
+    title: string;
+    href: string;
+    Icon: SvgIconComponent;
+}
+
+const MenuCard: React.FC<MenuCardProps> = React.memo((props: MenuCardProps) => {
+    return (
+        <Grid item xs={6} md={4}>
+            <Card>
+                <CardActionArea>
+                    <CardContent>
+                        <Box sx={{ display: "flex", justifyContent: "center" }}>
+                            <props.Icon fontSize="large" />
+                        </Box>
+                        <Box sx={{ display: "flex", justifyContent: "center" }}>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {props.title}
+                            </Typography>
+                        </Box>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
+    )
+});
 
 const Home: React.FunctionComponent = () => {
     return (
-        <IndexContainer title="學倉頡" linkList={linkList} />
+        <Container maxWidth="sm" sx={{ paddingTop: "25%", display: "flex", flexDirection: 'column', justifyContent: "center", alignContent: "center" }}>
+            <Box pb={4} sx={{ display: "flex", justifyContent: "center"}}>
+                <Typography variant="h2" component="h2">學倉頡</Typography>
+            </Box>
+            <Grid container spacing={2}>
+                {
+                    linkList.map((item) => {
+                        return (
+                            <MenuCard title={item.title} href={item.href} Icon={item.icon} />
+                        )
+                    })
+                }
+            </Grid>
+        </Container>
     )
 }
 

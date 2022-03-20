c4aba1a8e35584029f884d5a2faf8b81c7a61028
Gordon Chan <altg.star@gmail.com>
2022-03-21 00:51:29 +0800

    update: fix css

--- src/components/base/BaseContainer.tsx
+++ src/components/base/BaseContainer.tsx
@@ -15,7 +15,7 @@ type BaseContainerProps = {
 
 const BaseContainer: React.FunctionComponent<BaseContainerProps> = React.memo((props: BaseContainerProps) => {
     return (
-        <Container maxWidth="md" disableGutters sx={{ height: "100vh", position: "relative", display: "flex", flexDirection: "column" }}>
+        <Container maxWidth="md" disableGutters sx={{ height: "100vh", maxHeight: "-webkit-fill-available", position: "relative", display: "flex", flexDirection: "column" }}>
             <MenuBar title={props.title} subtitle={props.subtitle} backOnClick={props.backOnClick} />
             <Container sx={{ display: "flex", height: "80vh" }}>{props.children}</Container>
             <Container disableGutters sx={{ display: "flex" }}><KeyboardLayout /></Container>
